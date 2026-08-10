4fa84f5d17b2dcfcc454ce0041a7235a0e08aed3
Chijioke Joseph <chijiokejoseph2022@gmail.com>
2026-08-10 10:16:17 +0100

    commited

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vitest/config';
 import swc from 'unplugin-swc';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   test: {
@@ -16,7 +15,6 @@ export default defineConfig({
     },
   },
   plugins: [
-    tsconfigPaths(),
     swc.vite({
       module: { type: 'es6' },
     }),
